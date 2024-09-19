b4168fbf69505e6058d9be378ae082afcbace6cd
Marcela Marina <163910094+MarcelaMarinaBeaz@users.noreply.github.com>
2024-09-19 16:32:06 -0300

    hg

--- TrabalhoFinal/02-Repository/Data/InicializadorBd.cs
+++ TrabalhoFinal/02-Repository/Data/InicializadorBd.cs
@@ -43,6 +43,14 @@ namespace TrabalhoFinal._02_Repository.Data
                  Quantidade TEXT NOT NULL,
                  Estoque TEXT NOT NULL
                 );";
+                commandoSQL += @"   
+                 CREATE TABLE IF NOT EXISTS Joias(
+                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 Preco TEXT NOT NULL,
+                 Quantidade TEXT NOT NULL,
+                 Estoque TEXT NOT NULL
+                );";
+
                 using (var command = new SQLiteCommand(commandoSQL, connection))
                 {
                     command.ExecuteNonQuery();
