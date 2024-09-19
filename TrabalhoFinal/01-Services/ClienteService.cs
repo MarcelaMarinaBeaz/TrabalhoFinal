411d2a7ab65848bea21c72270dc505d47e95bf2f
Marcela Marina <163910094+MarcelaMarinaBeaz@users.noreply.github.com>
2024-09-19 17:05:34 -0300

    Create ClienteService.cs

---
+++ TrabalhoFinal/01-Services/ClienteService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabalhoFinal._02_Repository;
+using TrabalhoFinal._03_Entidades;
+
+namespace TrabalhoFinal._01_Services;
+
+public class ClienteService
+{
+
+    public ClienteRepository repository { get; set; }
+
+    public ClienteService(string _config)
+    {
+        repository = new ClienteRepository(_config);
+    }
+
+    public void Adicionar(Cliente i)
+    {
+        repository.Adicionar(i);
+    }
+    public void Remover(int id)
+    {
+        Cliente i = BuscarIId(id);
+        repository.Remover(i.ID);
+    }
+    public Cliente BuscarIId(int id)
+    {
+        return repository.BuscarPorId(id);
+    }
+    public void Editar(int id, Cliente i)
+    {
+        repository.Editar(id, i);
+    }
+    public List<Cliente> Listar()
+    {
+        return repository.Listarr();
+    }
+}
